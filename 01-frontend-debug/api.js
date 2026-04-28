4408277790d51e3f3495a76b50a1508fe377dcea
SantiagoCastro25 <sact0429@gmail.com>
2026-04-28 14:16:45 -0500

    fix: we add a correct validation for userid, it cannot be negative or zero

--- 01-frontend-debug/api.js
+++ 01-frontend-debug/api.js
@@ -5,3 +5,4 @@ async function fetchUser(id) {
   const data = response.json();                                                 
   return data;
 }
+ 
--- 01-frontend-debug/app.js
+++ 01-frontend-debug/app.js
@@ -6,12 +6,12 @@ async function loadUser() {
   const userId = document.getElementById('userId').value;
 
  
-  if (userId = '') {           
+  if (userId === '') { //Aqui se corrige la validacion ya que no estaba haciendo una comparacion sino asignando '' a user id    
     showResult('Please enter a valid ID');
     return;
   }
 
-  if (userId > 0 === false) {  
+  if (userId <= 0) {  //Hacemos la validacion de manera correcta, ya que el id no puede ser negativo ni cero
     showResult('ID must be positive', true);
     return;
   }
@@ -32,3 +32,4 @@ function showResult(message, isError = false) {
   el.className = isError ? 'error' : '';
   el.textContent = message;
 }
+ 
